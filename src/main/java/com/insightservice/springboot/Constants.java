5896aff69fe40dbe8ee5e0a4164b03ecfd2b1725
fyffep <pgf01@rocketmail.com>
2022-01-30 15:17:28 -0500

    Added MongoDB and temp user classes

--- src/main/java/com/insightservice/springboot/Constants.java
+++ src/main/java/com/insightservice/springboot/Constants.java
@@ -3,4 +3,22 @@ package com.insightservice.springboot;
 public class Constants
 {
     public static final String REPO_STORAGE_DIR = "repositories";
+
+    // Default Branches
+    public static final String[] DEFAULT_BRANCHES = {"development", "master", "main"};
+
+    public static final GroupingMode DEFAULT_GROUPING = GroupingMode.PACKAGES;
+    public enum GroupingMode {
+        COMMITS,
+        PACKAGES
+    }
+
+    // Heat
+    public static final int HEAT_MIN = 1;
+    public static final int HEAT_MAX = 10;
+
+
+    //Prevent instantiation
+    private Constants() {
+    }
 }
