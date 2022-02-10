2069206cee00354d7b997842607b058f2f9f57b2
fyffep <pgf01@rocketmail.com>
2022-02-10 17:58:47 -0500

    Added test case files back; they may need adjustment

---
+++ src/test/java/intellij_extension/utility/HeatCalculationUtilityTest.java
@@ -0,0 +1,20 @@
+package intellij_extension.utility;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static intellij_extension.utility.RepositoryAnalyzerTest.PROJECT_ROOT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HeatCalculationUtilityTest
+{
+    /*@Test
+    void calculateColorOfHeat()
+    {
+        final int TEST_HEAT_LEVEL = 1;
+        Color colorString = HeatCalculationUtility.colorOfHeat(TEST_HEAT_LEVEL); //method under test
+        assertEquals("0xff0000ff", colorString.toString());
+    }*/
+}
